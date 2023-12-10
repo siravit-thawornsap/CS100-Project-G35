e24d648598e559c7ef99dc35106b553e63d08cee
Ledomnat <prad.won@dome.tu.ac.th>
2023-12-10 20:37:21 +0700

    V1.5

--- script.js
+++ script.js
@@ -207,6 +207,39 @@ function validateFormOnInput() {
       console.error("An error occurred while submitting form data:", error);
     }
   }
+    function showInput() {
+      var name = document.getElementById("fullname").value;
+      var studentID = document.getElementById("studentID").value;
+      var universityEmail = document.getElementById("email").value;
+      var workTitle = document.getElementById("workTitle").value;
+      var workType = document.getElementById("activityType").value;
+      var academicYear = document.getElementById("academicYear").value;
+      var semester = document.getElementById("semester").value;
+      var startDate = document.getElementById("startDate").value;
+      var endDate = document.getElementById("endDate").value;
+      var location = document.getElementById("location").value;
+      var description = document.getElementById("description").value;
+    
+      if (name &&studentID &&universityEmail &&workTitle &&workType 
+        &&academicYear &&semester &&startDate &&endDate &&location ||description) {
+          var result =
+          "<p>Firstname and Lastname: " +name +"</p>" +
+          "<p>Student ID: " +studentID +"</p>" +
+          "<p>University Email: " +universityEmail +"</p>" +
+          "<p>Work/Activity Title: " +workTitle +"</p>" +
+          "<p>Type of Work/Activity: " +workType +"</p>" +
+          "<p>Academic Year: " +academicYear +"</p>" +
+          "<p>Semester: " +semester +"</p>" +
+          "<p>Start Date/Time: " +startDate +"</p>" +
+          "<p>End Date/Time: " +endDate +"</p>" +
+          "<p>Location: " +location +"</p>" +
+          "<p>Description: " +description +"</p>";
+    
+          display_message.innerHTML = result;
+      } else {
+        alert("Please fill in all required information.");
+      }
+    }
   
 // Event listener for form submission
 document.getElementById("myForm").addEventListener("submit", function (event) {
